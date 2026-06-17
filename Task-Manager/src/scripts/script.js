c55019ba56bc5117ad4b8e5bfe1e8cde1b3ee5ad
anshit-rangra <anshit22rangra@gmail.com>
2026-06-17 09:47:40 +0530

    delete and completed

--- Task-Manager/src/scripts/script.js
+++ Task-Manager/src/scripts/script.js
@@ -1,3 +1,16 @@
+let taskListElem = document.querySelector(".tasks-list")
+
+
+const taskData = getLocal("tasks") || []
+
+
+taskData.forEach((task, idx) => {
+    createTaskElement(task)
+})
+
+
+
+
 let createTaskButton = document.querySelector("nav button")
 let formSection = document.querySelector("#form-section")
 
@@ -19,18 +32,23 @@ let form = document.querySelector(".form form")
 let task = document.querySelector(".form form input")
 let option = document.querySelector(".form form select")
 
-let tasks = []
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
+
     
     let obj = {
+        id: taskData.length,
         task : task.value,
-        option: option.value
+        priority: option.value,
+        completed: false
     }
-    tasks.push(obj)
 
     createTaskElement(obj)
+    
+    taskData.push(obj)
+
+    setLocal(taskData)
 
 
     formSection.style.display = "none"
@@ -40,20 +58,82 @@ form.addEventListener("submit", function(e) {
 function createTaskElement(data) {
 
 
-let taskListElem = document.querySelector(".tasks-list")
 
     let li = document.createElement("li")
     li.innerHTML = `
-            <span>${data.task}</span>
+            <div class="task-info">
+                <span class="task-name">${data.task}</span>
+                <button class="priority ${data.priority}-priority">${data.priority}</button>
+            </div>
 
             <div class="actions">
-                <button class="complete">✓</button>
-                <button class="edit">Edit</button>
-                <button class="delete">Delete</button>
+                <button data-type="complete" class="complete">✓</button>
+                <button data-type="edit" class="edit">Edit</button>
+                <button data-type="delete" class="delete">Delete</button>
             </div>
     `
+
+    li.dataset.id = taskData.length
+
+    if(data?.completed){
+        li.querySelector(".task-name").style.textDecoration = "line-through"
+    }
 
     li.classList.add("task")
     taskListElem.appendChild(li)
 
-}``
+}
+
+
+taskListElem.addEventListener("click", function(e){
+    
+    if(e.target.dataset.type === "complete"){
+        setComplete(e.target)
+
+    } else if (e.target.dataset.type === "delete"){
+        deleteTask(e.target)
+    }
+
+})
+
+function setComplete(elem){
+    let grandParent = elem.parentElement.parentElement
+    grandParent.querySelector(".task-name").style.textDecoration = "line-through"
+
+    let id = grandParent.dataset.id;
+    
+    taskData[id].completed = !taskData[id].completed;
+    
+
+    setLocal(taskData)
+}
+
+function deleteTask(elem){
+
+    let grandParent = elem.parentElement.parentElement
+    let id = grandParent.dataset.id;
+
+
+    grandParent.remove()
+
+    console.log(id)
+
+    taskData.splice(id, 1)
+
+    console.log(taskData)
+
+    setLocal(taskData)
+
+}
+
+
+function setLocal(data) {
+    data = JSON.stringify(data)
+    localStorage.setItem("tasks", data)
+}
+
+function getLocal(key){
+    let data = localStorage.getItem(key)
+    data = JSON.parse(data)
+    return data
+}
